49cb2d170a206bce92ae9041360dac7794f657ff
Tsubasahane <wozaiha@gmail.com>
2025-02-24 18:24:10 +0800

    Chat Toggle

--- MareSynchronos/UI/SyncshellAdminUI.cs
+++ MareSynchronos/UI/SyncshellAdminUI.cs
@@ -452,6 +452,18 @@ public class SyncshellAdminUI : WindowMediatorSubscriberBase
                         _ = _apiController.GroupDelete(new(GroupFullInfo.Group));
                     }
                     UiSharedService.AttachToolTip("按住CTRL+hift并点击删除同步贝." + Environment.NewLine + "警告: 该操作无法取消.");
+
+                    if (UiSharedService.IsSupporter(_apiController.UID))
+                    {
+                        ImGui.Spacing();
+                        ImGui.Separator();
+                        var enabled = GroupFullInfo.GroupPermissions.IsEnabledChat();
+                        if (ImGui.Checkbox("开启同步贝内聊天功能", ref enabled))
+                        {
+                            perm.SetEnabledChat(enabled);
+                            _ = _apiController.GroupChangeGroupPermissionState(new(GroupFullInfo.Group, perm));
+                        }
+                    }
                 }
                 ownerTab.Dispose();
             }
